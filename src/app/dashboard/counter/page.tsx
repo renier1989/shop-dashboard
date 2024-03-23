f664ba02e45529b5bcf4f50ac2ff806a10a8fe33
Renier Vargas <renier1989@gmail.com>
2024-03-23 08:17:57 -0500

    Creando conflicto entre compoenentes del lado del cliente y el servidor al mismo tiempo (ERROR)

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,8 +1,25 @@
+'use client'
+import { Metadata } from "next";
+import { useState } from "react";
+
+
+export const metadata:Metadata = {
+ title: 'Counter Page',
+ description: 'Countar de productos',
+};
 
 export default function CounterPage() {
+  const [count, setCount] = useState(10);
+  
   return (
-    <div>
-      <h1>Hello Page - This is the Counter Page</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Simulacion de productos en el Carrito</span>
+      <span className="text-8xl font-semibold">{count}</span>
+
+      <div className="flex gap-1">
+        <button className="flex items-center justify-center p-2 w-[100px] rounded text-white bg-indigo-950 transition-all hover:bg-indigo-400 hover:text-indigo-950 duration-200 font-semibold text-lg" onClick={()=>setCount(count - 1)}>-1</button>
+        <button className="flex items-center justify-center p-2 w-[100px] rounded text-white bg-indigo-950 transition-all hover:bg-indigo-400 hover:text-indigo-950 duration-200 font-semibold text-lg" onClick={()=>setCount(count + 1)} >+1</button>
+      </div>
     </div>
   );
 }
