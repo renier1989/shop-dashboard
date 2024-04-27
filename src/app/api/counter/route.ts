68f7e4cae4a3c575168591cfc75340960efa2ae9
Renier Vargas <renier1989@gmail.com>
2024-04-27 07:42:52 -0500

    Creando un end point base

---
+++ src/app/api/counter/route.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from "next/server"
+
+export const dynamic = 'force-dynamic' // defaults to auto
+export async function GET(request: Request) {
+    return NextResponse.json({method:'GET' ,counter: '1000' }, { status: 200 })
+}
+export async function POST(request: Request) {
+    return NextResponse.json({method:'POST' , counter: '1000' }, { status: 200 })
+}
